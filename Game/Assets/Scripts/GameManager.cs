3448587e10246c77925d5b766d8bf270d54df43d
Gabriel Knoll <gabriel.knoll@icloud.com>
2019-12-07 17:48:04 +0100

    First implementation of GameManager

---
+++ Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameState { Intro, PLAY }
+
+public delegate void OnStateChangeHandler();
+
+public class GameManager: MonoBehaviour 
+{
+	protected GameManager() { }
+	private static GameManager instance = null;
+	public event OnStateChangeHandler OnStateChange;
+    public GameState gameState { get; private set; }
+
+    public static GameManager Instance
+	{
+        get
+		{
+            if (GameManager.instance == null)
+			{
+				DontDestroyOnLoad(GameManager.instance);
+				GameManager.instance = new GameManager();
+			}
+			return GameManager.instance;
+		}
+	}
+
+    public void SetGameState(GameState state)
+	{
+		this.gameState = state;
+		OnStateChange();
+	}
+
+    public void OnApplicationQuit()
+	{
+		GameManager.instance = null;
+	}
+
+}
