9893e4120d9d1bfe3968bf9372e21feaab9ab3bc
Gabriel Knoll <gabriel.knoll@icloud.com>
2019-12-08 02:49:05 +0100

    Settings

---
+++ Game/Assets/UI/UIManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerScript : MonoBehaviour
+{
+    public Animator dialog;
+    public Animator settings;
+    public Animator start;
+    public Animator quit;
+
+    public void openSettings()
+    {
+        settings.SetBool("isHidden", true);
+        start.SetBool("isHidden", true);
+        quit.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void closeSettings()
+    {
+        settings.SetBool("isHidden", false);
+        start.SetBool("isHidden", false);
+        quit.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
+    }
+}
