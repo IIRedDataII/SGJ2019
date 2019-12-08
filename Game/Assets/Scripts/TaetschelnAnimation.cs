846f05f3a438700b3011451f4adebc620c3b2440
SintiGameDev <ivan.sinitin@gmx.de>
2019-12-08 07:46:03 +0100

    Endszenen eingefuegt

---
+++ Game/Assets/Scripts/TaetschelnAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaetschelnAnimation : MonoBehaviour
+{
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (true)
+        {
+            anim.SetInteger("Timer", 2);//2 bel. Wert
+        }
+        else {
+            anim.SetInteger("Timer", 2);
+        }
+    }
+}
